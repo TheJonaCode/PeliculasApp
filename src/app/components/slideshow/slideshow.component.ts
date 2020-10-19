e0ec7806a05ef87264453e25be9bfc90ec550ffc
Jonathan <mcjhona069@gmail.com>
2020-10-19 16:47:47 -0500

    250 Swiper Sideshow de Películas

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,16 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
 
   @Input() movies: Movie[];
 
   constructor() { }
+
+  ngAfterViewInit(): void {
+    const mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+  })
+
+  }
 
   ngOnInit(): void {
     console.log(this.movies);
