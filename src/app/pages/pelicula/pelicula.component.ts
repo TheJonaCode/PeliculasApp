35edfb2e2cadfaa52975d41ae6adecee9ab12df8
Jonathan <mcjhona069@gmail.com>
2020-10-19 22:03:29 -0500

    264 RXJS - CombineLatest

--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -5,6 +5,7 @@ import { MovieDetailsResponse } from '../../interfaces/movie-response';
 import { Movie } from '../../interfaces/cartelera-response';
 import { Location } from '@angular/common';
 import { Cast } from '../../interfaces/credits-response';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-pelicula',
@@ -23,20 +24,38 @@ export class PeliculaComponent implements OnInit {
 
   ngOnInit(): void {
     const { id }=this.activatedRouter.snapshot.params;
-        this.peliculasService.getPeliculaDetalle(id).subscribe(movie=>{
 
-          this.pelicula=movie;
-          if (!movie) {
-            this.router.navigateByUrl('/home');
-            return;
-          }
-          this.pelicula=movie;
-        });
-    
-    
-        this.peliculasService.getCast(id).subscribe(cast=>{
-          this.cast=cast;
-          this.cast=cast.filter(actor=>actor.profile_path !== null);
-        })
-    
+    combineLatest([
+      this.peliculasService.getPeliculaDetalle(id),
+      this.peliculasService.getCast(id)
+    ]).subscribe(([pelicula, cast])=>{
+      if (!pelicula) {
+        this.router.navigateByUrl('/home');
+        return;
       }
+      this.pelicula=pelicula;
+      this.cast=cast.filter(actor=>actor.profile_path !== null);
+
+    });
+
+/*  this.peliculasService.getPeliculaDetalle(id).subscribe(movie=>{
+      this.pelicula=movie;
+      if (!movie) {
+        this.router.navigateByUrl('/home');
+        return;
+      }
+      this.pelicula=movie;
+    });
+
+    this.peliculasService.getCast(id).subscribe(cast=>{
+      this.cast=cast;
+      this.cast=cast.filter(actor=>actor.profile_path !== null);
+    })
+
+*/  
+  }
+
+  onRegresar(){
+    this.location.back();
+  }
+}
