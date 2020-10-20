0a578598cfb8f4b277283f267697bc2bc5814e9d
Jonathan <mcjhona069@gmail.com>
2020-10-19 20:34:45 -0500

    255 InfiniteScroll de películas

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -2,17 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CarteleraResponse } from '../interfaces/cartelera-response';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculasService {
 
+  private baseUrl: string='https://api.themoviedb.org/3';
+  private carteleraPage=1;
+
   constructor( private http: HttpClient ) {}
 
-  getCartelera():Observable<CarteleraResponse>{
-
-    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=059924efef2fae6fb36850399ab9b10d&language=es-ES&page=1');
+  get params(){
+    return{
+      api_key: '059924efef2fae6fb36850399ab9b10d',
+      language: 'es-ES',
+      page: this.carteleraPage.toString()
+    }
   }
 
+  getCartelera():Observable<CarteleraResponse>{
+    return this.http.get<CarteleraResponse>(`${this.baseUrl}/movie/now_playing`,{
+      params: this.params
+    }).pipe(
+      tap( ()=>{
+        this.carteleraPage+=1;
+      })
+    );
+
+  }
 }
